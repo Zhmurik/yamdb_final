2e331eea31de994eac3844a7a2adf5622eaf5daa
zhmurik <azhmurkova@gmail.com>
2021-12-03 00:11:16 -0800

    wip

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -90,6 +90,13 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', default="5432")
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -129,10 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
